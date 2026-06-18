16560e87e8e8a1dde3df5cb7ebcdb5a275fea6a3
bikramjit7777 <bikramjit7777@gmail.com>
2026-06-18 14:16:24 -0400

    split_text method to utils

--- utils.py
+++ utils.py
@@ -7,3 +7,9 @@ def extract_text_from_pdf(file):
         text += page.extract_text()
     return text
 
+def split_text(text, chunk_size=300):
+    words = text.split()
+    chunks = []
+    for i in range(0, len(words), chunk_size):
+        chunks.append(" ".join(words[i:i+chunk_size]))
+    return chunks
